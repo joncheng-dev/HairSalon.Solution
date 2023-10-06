a5ce26ac20307307eb40086d0a753806c330cd48
Jonathan Cheng <jcheng461@gmail.com>
2023-10-06 11:06:31 -0700

    update client controller and create view to allow user to add new clients

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client entry)
+    {
+      _db.Clients.Add(entry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
